81fae04a3ddb65deffee937879d87c7a395632a5
Emanuele Disco <emawind84@gmail.com>
2017-03-02 15:39:15 +0900

    update

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
-# Hub for multi PMIS instances
+# Hub for PMIS instances
+
+Thss web server is used as a controller for several PMIS application running on the same server.
+
+## Configuration
+
+Under the folder `hub/conf.d` there are two examples of configuration for one PMIS application.
+
+**backed1.conf.sample** - This is used for *SSL/TLS* connection with redirection from port *80* to *443*.
+
+**backed2.conf.sample** - This is the simple *HTTP* connection.
+
+Make a copy of one of these example, you can rename it with the name of the application.
+Inside you need to change the *dns* (the name after the directive `server_name`) 
+and the upstream server name (the name after the directive set `$upstream_webapp`).
+
+The upstream name is the value you gave to the variable [`HUB_INSTANCE`](https://github.com/sangahco/docker-pmis-app/blob/master/README.md "PMIS deploy guide")
+
+---
 
 Before running the service create the network with the following command:
 
     $ docker network create hub_net
+
+## Run using `docker-auto` script
+
+This is the recommended way to run this service, start the service with the following command:
+
+    $ ./docker-auto --prod up
+
+or if you are editing and testing the service with:
+
+    $ ./docker-auto --dev up
+
+See the help for more commands:
+
+    $ ./docker-auto --help
+
+
+## Run with `docker-compose`
+
 
 Then start the service with:
 
---
+++ docker-auto.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT_NAME="$PROJECT_NAME"
+if [ -z "$PROJECT_NAME" ]; then
+    PROJECT_NAME="$(cat .env | awk 'BEGIN { FS="="; } /^PROJECT_NAME/ {sub(/\r/,"",$2); print $2;}')"
+fi
+REGISTRY_URL="$REGISTRY_URL"
+if [ -z "$REGISTRY_URL" ]; then
+    REGISTRY_URL="$(cat .env | awk 'BEGIN { FS="="; } /^REGISTRY_URL/ {sub(/\r/,"",$2); print $2;}')"
+fi
+
+SCRIPT_BASE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$SCRIPT_BASE_PATH"
+
+usage() {
+echo "Usage:  $(basename "$0") [MODE] [OPTIONS] [COMMAND]"
+echo 
+echo "Mode:"
+echo "  --prod          Production mode"
+echo "  --dev           Development mode"
+echo
+echo "Options:"
+echo "  --help          Show this help message"
+echo
+echo "Commands:"
+echo "  up              Start the services"
+echo "  down            Stop the services"
+echo "  ps              Show the status of the services"
+echo "  logs            Follow the logs on console"
+echo "  remove-all      Remove all containers"
+echo "  stop-all        Stop all containers running"
+echo "  build           Build the image"
+echo "  publish         Publish the image to the registry"
+}
+
+CONF_ARG="-f docker-compose.yml"
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+for i in "$@"
+do
+case $i in
+    --prod)
+        CONF_ARG="-f docker-compose.yml"
+        shift
+        ;;
+    --dev)
+        CONF_ARG="-f docker-compose-dev.yml"
+        shift
+        ;;
+    --help|-h)
+        usage
+        exit 1
+        ;;
+    *)
+        ;;
+esac
+done
+
+echo "Arguments: $CONF_ARG"
+echo "Command: $@"
+
+if [ "$1" == "up" ]; then
+    docker-compose $CONF_ARG pull
+    docker-compose $CONF_ARG build --pull
+    docker-compose $CONF_ARG up -d --remove-orphans
+    exit 0
+
+elif [ "$1" == "stop-all" ]; then
+    if [ -n "$(docker ps --format {{.ID}})" ]
+    then docker stop $(docker ps --format {{.ID}}); fi
+    exit 0
+
+elif [ "$1" == "remove-all" ]; then
+    if [ -n "$(docker ps -a --format {{.ID}})" ]
+    then docker rm $(docker ps -a --format {{.ID}}); fi
+    exit 0
+
+elif [ "$1" == "logs" ]; then
+    shift
+    docker-compose $CONF_ARG logs -f --tail 200 "$@"
+    exit 0
+
+elif [ "$1" == "build" ]; then
+    if [ -z "$REGISTRY_URL" ]; then echo "REGISTRY_URL not defined."; exit 1; fi
+    if [ -z "$PROJECT_NAME" ]; then echo "PROJECT_NAME not defined."; exit 1; fi
+    
+    docker build -t $REGISTRY_URL/$PROJECT_NAME was
+    exit 0
+
+elif [ "$1" == "publish" ]; then
+    if [ -z "$REGISTRY_URL" ]; then echo "REGISTRY_URL not defined."; exit 1; fi
+    if [ -z "$PROJECT_NAME" ]; then echo "PROJECT_NAME not defined."; exit 1; fi
+    
+    docker login $REGISTRY_URL
+    docker push $REGISTRY_URL/$PROJECT_NAME
+    exit 0
+fi
+
+docker-compose $CONF_ARG "$@"
