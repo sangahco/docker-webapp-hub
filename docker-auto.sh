872e2cdb8a887232263c209ba9ad0e65a20807ef
Emanuele Disco <emawind84@gmail.com>
2017-04-15 19:07:29 +0900

    update

--- docker-auto.sh
+++ docker-auto.sh
@@ -54,6 +54,7 @@ echo "  up              Start the services"
 echo "  down            Stop the services"
 echo "  ps              Show the status of the services"
 echo "  logs            Follow the logs on console"
+echo "  login           Log in to a Docker registry"
 echo "  remove-all      Remove all containers"
 echo "  stop-all        Stop all containers running"
 echo "  build           Build the image"
@@ -88,7 +89,11 @@ done
 echo "Arguments: $CONF_ARG"
 echo "Command: $@"
 
-if [ "$1" == "up" ]; then
+if [ "$1" == "login" ]; then
+    docker login $REGISTRY_URL
+    exit 0
+
+elif [ "$1" == "up" ]; then
     on_run
     docker-compose $CONF_ARG pull
     docker-compose $CONF_ARG build --pull
